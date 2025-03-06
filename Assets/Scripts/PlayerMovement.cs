84fa1980b226dd4aa3ae9fbdc17e5f764fbf5b64
Jaron Lin <jaronslin@gmail.com>
2025-03-06 13:05:21 -0800

    horizontal movement and side scrolling

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private new Camera camera;
+    private new Rigidbody2D rigidbody;
+
+    private Vector2 velocity;
+    private float inputAxis;
+
+    public float moveSpeed = 8f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        camera = Camera.main;
+    }
+
+    private void Update() //checking input
+    {
+        HorizontalMovement();
+    }
+
+    private void HorizontalMovement() //check input in horizontal axis
+    {
+        inputAxis = Input.GetAxis("Horizontal");
+        velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, moveSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody.position;
+        position += velocity * Time.fixedDeltaTime;
+
+        Vector2 leftEdge = camera.ScreenToWorldPoint(Vector2.zero);
+        Vector2 rightEdge = camera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        position.x = Mathf.Clamp(position.x, leftEdge.x + 0.5f, rightEdge.x - 0.5f);
+
+        rigidbody.MovePosition(position);
+    }
+}
---
+++ Assets/Scripts/SideScrolling.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SideScrolling : MonoBehaviour
+{
+    private Transform player;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void LateUpdate() //called last before things are rendered to screen, tracking updated
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
+        transform.position = cameraPosition;
+    }
+}
